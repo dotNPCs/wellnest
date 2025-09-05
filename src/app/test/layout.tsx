12825aa6fc8d4fcaefd39f7c2cb9b9f326fc0f2f
4rchx824 <archielaw824@gmail.com>
2025-09-05 16:38:18 +0800

    refactor: clean up code formatting and remove unused imports

--- src/app/test/layout.tsx
+++ src/app/test/layout.tsx
@@ -1,12 +1,12 @@
+import type React from "react";
 import { api } from "@/trpc/server";
 import Page from "./page";
-import type React from "react";
 
 const Layout = async () => {
   const pet = await api.llm.findFirstPet();
 
   return (
-    <div className="p-4 h-screen">
+    <div className="h-screen p-4">
       <Page pet={pet} />
     </div>
   );
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { TRPCError, type inferProcedureOutput } from "@trpc/server";
+import { type inferProcedureOutput } from "@trpc/server";
 import type { appRouter } from "@/server/api/root";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -7,7 +7,6 @@ import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
 import { api } from "@/trpc/react";
 import { TRPCClientError } from "@trpc/client";
-import { c } from "node_modules/framer-motion/dist/types.d-Cjd591yU";
 type UserPet = inferProcedureOutput<(typeof appRouter)["llm"]["findFirstPet"]>;
 
 type Props = {
@@ -17,7 +16,7 @@ const Page = ({ pet }: Props) => {
   const [query, setQuery] = useState<string>("");
   const [LLMResponse, setLLMResponse] = useState<string>("");
 
-  const { data, isLoading, refetch } = api.llm.chat.useQuery(
+  const { isLoading, refetch } = api.llm.chat.useQuery(
     {
       message: query,
     },
@@ -26,9 +25,9 @@ const Page = ({ pet }: Props) => {
 
   const handleSubmit = async () => {
     try {
-      const { data, error } = await refetch();
-
-      if (error) throw error;
+      const { data } = await refetch({
+        throwOnError: true,
+      });
 
       if (data) setLLMResponse(data);
     } catch (e) {
@@ -36,7 +35,9 @@ const Page = ({ pet }: Props) => {
       if (e instanceof TRPCClientError) {
         setLLMResponse(e.message);
 
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         if (e?.data?.zodError?.fieldErrors?.message?.length > 0)
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
           setLLMResponse(e.data.zodError.fieldErrors.message[0]);
       }
     }
