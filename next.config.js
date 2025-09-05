a5141c5c25790e3db18cdcc15addcd7cbb8dccb2
Allison <allison.loo.2025@computing.smu.edu.sg>
2025-09-06 03:15:49 +0800

    test

--- next.config.js
+++ next.config.js
@@ -1,37 +1,37 @@
-import "./src/env.js";
+import "@/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
     unoptimized: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
-              style-src 'self' 'unsafe-inline' https:;
-              img-src 'self' data: https:;
-              connect-src 'self' https:;
-              font-src 'self' https:;
-              object-src 'none';
-              frame-ancestors 'none';
-            `.replace(/\n/g, " "),
-          },
-          { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "X-Frame-Options", value: "DENY" },
-          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-          { key: "Permissions-Policy", value: "geolocation=(), microphone=()" },
-          { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "Content-Security-Policy",
+  //           value: `
+  //             default-src 'self';
+  //             script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
+  //             style-src 'self' 'unsafe-inline' https:;
+  //             img-src 'self' data: https:;
+  //             connect-src 'self' https:;
+  //             font-src 'self' https:;
+  //             object-src 'none';
+  //             frame-ancestors 'none';
+  //           `.replace(/\n/g, " "),
+  //         },
+  //         { key: "X-Content-Type-Options", value: "nosniff" },
+  //         { key: "X-Frame-Options", value: "DENY" },
+  //         { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+  //         { key: "Permissions-Policy", value: "geolocation=(), microphone=()" },
+  //         { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default config;
