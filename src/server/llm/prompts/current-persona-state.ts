8e095d050db7a8a81f1010b757e0e33a9ddbdf8b
dongen <dongenchua03@gmail.com>
2025-09-06 13:23:09 +0800

    moodUPdate

--- src/server/llm/prompts/current-persona-state.ts
+++ src/server/llm/prompts/current-persona-state.ts
@@ -3,7 +3,10 @@ export const getCurrentPersonaStatePrompt = (persona: object) => {
     [Persona.json]
     ${JSON.stringify(persona)}
 
-    Use the above persona, generate for me the following and adhere to the format
+    Use the above persona, generate for me the following and adhere to the format.
+    Make the mood more sensitive/concerned for the user when time minutes is even, and make the dialouge a suggestion/reminder when pet is sensitive/concerned.
+    The dialouge should be third person e.g Your pet thinks you should do something.
+
     {
         "mood": "<Generate Response Here, Max 80 chars>",
         "dialogue": "<Generate Response Here, Max 70 chars>",
